2bb5f90654d28e58dceb56ae10c081d4975e9ba9
islomabduqodir <islomabduqodir@gmail.com>
2022-01-21 21:02:42 +0500

    login

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@ $(".complaint__content").hide();
 $(".complaint").click(function () { 
     $(".complaint__content").show();
 });
+$(".exit1").click(function () { 
+    $(".complaint__content").hide();
+});
 $(".investigation").click(function () { 
     $(".investigation__content").show();
+});
+$(".exit2").click(function () { 
+    $(".investigation__content").hide();
 });
 
