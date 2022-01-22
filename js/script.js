c3416ab792122d68416841e5c8025f539031e9ec
islomabduqodir <islomabduqodir@gmail.com>
2022-01-22 19:24:52 +0500

    full version

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 $(".investigation__content").hide();
 $(".complaint__content").hide();
+$(".comment__content").hide();
+$(".comment__content2").hide();
 $(".complaint").click(function () { 
     $(".complaint__content").show();
-});
-$(".exit1").click(function () { 
-    $(".complaint__content").hide();
 });
 $(".investigation").click(function () { 
     $(".investigation__content").show();
 });
-$(".exit2").click(function () { 
+$(".comment__btn").click(function () { 
+    $(".comment__content").show();
+});
+$(".comment__btn2").click(function () { 
+    $(".comment__content2").show();
+});
+$(".exit").click(function () { 
+    $(".complaint__content").hide();
     $(".investigation__content").hide();
+    $(".comment__content").hide();
+    $(".comment__content2").hide();
 });
 
